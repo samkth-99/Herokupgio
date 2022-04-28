6cd67d06312612f51eb89b5185af0e320515e262
Thy Huynh <khanhhthyy99@gmail.com>
2022-04-28 12:51:10 -0700

    ver 1.1

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Add required packages
+const multer = require("multer");
+const upload = multer();
+
 require("dotenv").config()
+
 const express = require("express");
 const app = express();
 
@@ -24,11 +28,42 @@ app.get("/", (req, res) => {
             message = "success";
             model = result.rows;
         };
-    res.render("index", {
-        message: message,
-        model : model
+        res.render("index", {
+            message: message,
+            model : model
+        });
     });
 });
+
+app.get("/input", (req, res) => {
+    res.render("input");
+ });
+ 
+ app.post("/input",  upload.single('filename'), (req, res) => {
+     if(!req.file || Object.keys(req.file).length === 0) {
+         message = "Error: Import file not uploaded";
+         return res.send(message);
+     };
+     //Read file line by line, inserting records
+     const buffer = req.file.buffer; 
+     const lines = buffer.toString().split(/\r?\n/);
+ 
+     lines.forEach(line => {
+          //console.log(line);
+          product = line.split(",");
+          //console.log(product);
+          const sql = "INSERT INTO PRODUCT(prod_id, prod_name, prod_desc, prod_price) VALUES ($1, $2, $3, $4)";
+          pool.query(sql, product, (err, result) => {
+              if (err) {
+                  console.log(`Insert Error.  Error message: ${err.message}`);
+              } else {
+                  console.log(`Inserted successfully`);
+              }
+         });
+     });
+     message = `Processing Complete - Processed ${lines.length} records`;
+     res.send(message);
+ });
 
 // Add database package and connection string (can remove ssl)
 const { Pool } = require('pg');
@@ -37,5 +72,4 @@ const pool = new Pool({
   ssl: {
     rejectUnauthorized: false
   }
-});
 });
