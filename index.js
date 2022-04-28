367ff4d6d857f90ae293c150cd5b35a6526925bb
Thy Huynh <khanhhthyy99@gmail.com>
2022-04-28 11:29:43 -0700

    ver 1.0

---
+++ index.js
@@ -0,0 +1,41 @@
+// Add required packages
+require("dotenv").config()
+const express = require("express");
+const app = express();
+
+// Set up EJS
+app.set("view engine", "ejs");
+
+// Start listener
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server started (http://localhost:3000/) !");
+});
+
+// Setup routes
+app.get("/", (req, res) => {
+    //res.send ("Hello world...");
+    const sql = "SELECT * FROM PRODUCT ORDER BY PROD_ID";
+    pool.query(sql, [], (err, result) => {
+        var message = "";
+        var model = {};
+        if(err) {
+            message = `Error - ${err.message}`;
+        } else {
+            message = "success";
+            model = result.rows;
+        };
+    res.render("index", {
+        message: message,
+        model : model
+    });
+});
+
+// Add database package and connection string (can remove ssl)
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+});
