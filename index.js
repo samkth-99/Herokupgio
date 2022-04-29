a3e08872c4d3ef90e46f70df32df51afc71d6ac3
Thy Huynh <khanhhthyy99@gmail.com>
2022-04-28 20:39:45 -0700

    ver 1.3

--- index.js
+++ index.js
@@ -73,3 +73,28 @@ const pool = new Pool({
     rejectUnauthorized: false
   }
 });
+
+app.get("/output", (req, res) => {
+    var message = "";
+    res.render("output",{ message: message });
+   });
+   
+   
+   app.post("/output", (req, res) => {
+       const sql = "SELECT * FROM PRODUCT ORDER BY PROD_ID";
+       pool.query(sql, [], (err, result) => {
+           var message = "";
+           if(err) {
+               message = `Error - ${err.message}`;
+               res.render("output", { message: message })
+           } else {
+               var output = "";
+               result.rows.forEach(product => {
+                   output += `${product.prod_id},${product.prod_name},${product.prod_desc},${product.prod_price}\r\n`;
+               });
+               res.header("Content-Type", "text/csv");
+               res.attachment("export.csv");
+               return res.send(output);
+           };
+       });
+   });
